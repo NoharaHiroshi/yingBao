1bbaaf43259c0a1d5ed880bb9c31ab22f817b226
赵宇霆 <zhaoyuting@blockfundchain.cn>
2020-06-20 18:42:13 +0800

    增加了漫画阅读记录功能

--- views/website.py
+++ views/website.py
@@ -63,22 +63,32 @@ def chapter_list():
     chapter_list_info = list()
     context = {
         "name": '',
-        "comic_id": comic_id,
-        "chapter_list": chapter_list_info
+        "comic_id": str(comic_id),
+        "chapter_list": chapter_list_info,
+        "continue_chapter_id": "",
+        "continue_chapter_name": "",
+        "continue_page": 0
     }
     with get_session() as db_session:
         comic = db_session.query(Comic).get(comic_id)
         chapter_data = db_session.query(ComicChapter).filter(
             ComicChapter.comic_id == comic_id
         ).order_by(ComicChapter.index).all()
+        comic_read_record = db_session.query(ComicReadRecord).filter(
+            ComicReadRecord.comic_id == comic_id
+        ).first()
+        chapter_record = db_session.query(ComicChapter).get(comic_read_record.chapter_id)
         for chapter in chapter_data:
             chapter_info = {
-                "id": chapter.id,
+                "id": str(chapter.id),
                 "name": chapter.chapter_name,
                 "index": chapter.index
             }
             chapter_list_info.append(chapter_info)
     context["name"] = comic.name
+    context["continue_chapter_id"] = str(comic_read_record.chapter_id)
+    context["continue_chapter_name"] = chapter_record.chapter_name
+    context["continue_page"] = comic_read_record.page
     return render_template("comic_chapter_index.html", **context)
 
 
@@ -91,46 +101,67 @@ def comic_chapter_content():
         "comic_name": "",
         "chapter_name": "",
         "chapter_page": 0,
-        "img_url_list": img_url_list
+        "img_url_list": img_url_list,
+        "comic_id": str(comic_id),
+        "chapter_id": str(chapter_id),
+        "next_chapter_id": ""
     }
     with get_session() as db_session:
         comic = db_session.query(Comic).get(comic_id)
         chapter = db_session.query(ComicChapter).get(chapter_id)
+        next_chapter = db_session.query(ComicChapter).filter(
+            ComicChapter.index == chapter.index - 1
+        ).first()
     comic_base_path = os.path.join(os.path.join(os.path.dirname(os.path.dirname(__file__)), "static"), "comic")
     comic_path = os.path.join(comic_base_path, comic.name)
     chapter_name = "%s_%s" % (chapter.index, chapter.chapter_name)
     comic_chapter_path = os.path.join(comic_path, chapter_name)
     img_file_list = os.walk(comic_chapter_path)
     for root, dirs, files in img_file_list:
-        for f in files:
+        for i in range(len(files)):
+            f = files[i]
             url = "/static/comic/%s/%s/%s" % (comic.name, chapter_name, f)
             url = url.replace(" ", "%20")
-            img_url_list.append(url)
+            img_url_list.append({
+                "url": url,
+                "id": i
+            })
     context.update({
         "comic_name": comic.name,
         "chapter_name": chapter.chapter_name,
         "chapter_page": chapter.page_num,
-        "img_url_list": img_url_list
+        "img_url_list": img_url_list,
+        "next_chapter_id": str(next_chapter.id)
     })
     return render_template("comic_chapter_content.html", **context)
 
-# @website.route("/comic_chapter_content")
-# def comic_chapter():
-#     chapter_id = request.args.get('chapter_id')
-#     comic_id = request.args.get('comic_id')
-#     page = request.args.get("page")
-#     with get_session() as db_session:
-#
-#         # comic_read_record = db_session.query(ComicReadRecord).filter(
-#         #     ComicReadRecord.comic_id == comic_id
-#         # ).first()
-#         # if not comic_read_record:
-#         #     comic_read_record = ComicReadRecord()
-#         #     comic_read_record.comic_id = comic_id
-#         #     comic_read_record.chapter_id = chapter_id
-#
-#
-#     return render_template("comic_chapter_index.html", **context)
+
+@website.route("/comic_read_record", methods=['POST'])
+def comic_chapter():
+    result = {
+        "response": "success",
+        "info": ""
+    }
+    chapter_id = request.form.get('chapter_id')
+    comic_id = request.form.get('comic_id')
+    page = request.form.get("page")
+    with get_session() as db_session:
+
+        comic_read_record = db_session.query(ComicReadRecord).filter(
+            ComicReadRecord.comic_id == comic_id
+        ).first()
+        if not comic_read_record:
+            comic_read_record = ComicReadRecord()
+            comic_read_record.comic_id = comic_id
+            comic_read_record.chapter_id = chapter_id
+            comic_read_record.page = page
+            db_session.add(comic_read_record)
+        else:
+            comic_read_record.chapter_id = chapter_id
+            comic_read_record.page = page
+        db_session.commit()
+    return jsonify(result)
+
 
 if __name__ == "__main__":
     print os.path.dirname(__file__)
