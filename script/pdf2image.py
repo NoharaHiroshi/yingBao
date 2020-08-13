8220d8c9467dff2b3d2e32582eba1af8ef9a1df1
Lands <380788433@qq.com>
2020-08-13 16:15:43 +0800

    新增图片转换为pdf

--- script/pdf2image.py
+++ script/pdf2image.py
@@ -3,17 +3,18 @@
 import datetime
 import os
 import fitz
+import glob
 
 
-def pyMuPDF_fitz(pdfPath, imagePath):
+def pdf2pic(pdfPath, imagePath):
     print("imagePath=" + imagePath)
     pdf_doc = fitz.open(pdfPath)
     for pg in range(pdf_doc.pageCount):
         print "%s.jpg" % str(pg)
         page = pdf_doc[pg]
         rotate = int(0)
-        zoom_x = 1
-        zoom_y = 1
+        zoom_x = 2
+        zoom_y = 2
         mat = fitz.Matrix(zoom_x, zoom_y).preRotate(rotate)
         pix = page.getPixmap(matrix=mat, alpha=False)
 
@@ -23,7 +24,25 @@ def pyMuPDF_fitz(pdfPath, imagePath):
         pix.writePNG(imagePath + '/' + '%s.jpg' % pg)  # 将图片写入指定的文件夹内
 
 
+def pic2pdf(image_path, pdf_path):
+    doc = fitz.open()
+    for root, dirs, files in os.walk(image_path):
+        files.sort(key=lambda x: int(x.split(".")[0]))
+        print files
+        for i in range(len(files)):
+            img = os.path.join(root, files[i])
+            imgdoc = fitz.open(img)         # 打开图片
+            pdfbytes = imgdoc.convertToPDF()    # 使用图片创建单页的 PDF
+            imgpdf = fitz.open("pdf", pdfbytes)
+            doc.insertPDF(imgpdf)          # 将当前页插入文档
+    if os.path.exists(pdf_path):
+        os.remove(pdf_path)
+    doc.save(pdf_path)          # 保存pdf文件
+    doc.close()
+
+
 if __name__ == "__main__":
-    pdfPath = u''
-    imagePath = u''
-    pyMuPDF_fitz(pdfPath, imagePath)
+    # pdfPath = u'E:\\扫描\\2020-08-13_154502.pdf'
+    # imagePath = u'E:\\扫描\\反面'
+    # pyMuPDF_fitz(pdfPath, imagePath)
+    pic2pdf()
